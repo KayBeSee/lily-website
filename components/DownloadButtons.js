820c9512eec15b25135d26eeb8bb5bc5f50105de
Kevin Mulcrone <kaybesee@gmail.com>
2021-03-23 23:23:50 -0600

    fix download button text alignment

--- components/DownloadButtons.js
+++ components/DownloadButtons.js
@@ -5,42 +5,44 @@ const DownloadButtons = () => {
     <div className="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
       <a
         href="https://github.com/KayBeSee/lily-wallet/releases/download/v1.1-rc-1/Lily.Wallet.Setup.1.0.2.exe"
-        className="col-span-1 flex justify-center py-8 px-8 bg-gray-50 flex-col items-center mx-4 my-4 shadow-md rounded-2xl hover:-translate-y-1 transform duration-500 ease-in-out"
+        className="col-span-1 flex justify-center py-8 px-2 sm:px-8 bg-gray-50 flex-col items-center mx-4 my-4 shadow-md rounded-2xl hover:-translate-y-1 transform duration-500 ease-in-out"
       >
         <div>
           <img className="max-h-12" src="./windows-logo.png" alt="Workcation" />
         </div>
         <div className="flex flex-col mt-4 items-center justify-center">
           <span>Windows</span>
-          <span className="text-gray-500 text-xs">
+          <span className="text-gray-500 text-xxs sm:text-xs">
             Lily.Wallet.Setup.1.0.2.exe
           </span>
         </div>
       </a>
       <a
         href="https://github.com/KayBeSee/lily-wallet/releases/download/v1.1-rc-1/Lily.Wallet-1.0.2.AppImage"
-        className="col-span-1 flex justify-center py-8 px-8 bg-gray-50 flex-col items-center mx-4 my-4 shadow-md rounded-2xl hover:-translate-y-1 transform duration-500 ease-in-out"
+        className="col-span-1 flex justify-center py-8 px-2 sm:px-8 bg-gray-50 flex-col items-center mx-4 my-4 shadow-md rounded-2xl hover:-translate-y-1 transform duration-500 ease-in-out"
       >
         <div>
           <img className="max-h-12" src="./linux-logo.png" alt="Mirage" />
         </div>
         <div className="flex flex-col mt-4 items-center justify-center">
           <span>Linux</span>
-          <span className="text-gray-500 text-xs">
+          <span className="text-gray-500 text-xxs sm:text-xs">
             Lily.Wallet-1.0.2.AppImage
           </span>
         </div>
       </a>
       <a
         href="https://github.com/KayBeSee/lily-wallet/releases/download/v1.1-rc-1/Lily.Wallet-1.0.2.dmg"
-        className="col-span-1 flex justify-center py-8 px-8 bg-gray-50 flex-col items-center mx-4 my-4 shadow-md rounded-2xl hover:-translate-y-1 transform duration-500 ease-in-out"
+        className="col-span-1 flex justify-center py-8 px-2 sm:px-8 bg-gray-50 flex-col items-center mx-4 my-4 shadow-md rounded-2xl hover:-translate-y-1 transform duration-500 ease-in-out"
       >
         <div>
           <img className=" max-h-12" src="./apple-logo.png" alt="Tuple" />
         </div>
         <div className="flex flex-col mt-4 items-center justify-center">
           <span>Mac</span>
-          <span className="text-gray-500 text-xs">Lily.Wallet-1.0.2.dmg</span>
+          <span className="text-gray-500 text-xxs sm:text-xs">
+            Lily.Wallet-1.0.2.dmg
+          </span>
         </div>
       </a>
     </div>
