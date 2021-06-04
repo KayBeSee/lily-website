b028d4cdbf4d747f9e6558fcfa456ce7825ec762
Kevin Mulcrone <kaybesee@gmail.com>
2021-06-04 11:21:22 -0600

    update download buttons

--- components/DownloadButtons.js
+++ components/DownloadButtons.js
@@ -4,7 +4,7 @@ const DownloadButtons = () => {
   return (
     <div className="mt-6 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-8">
       <a
-        href="https://github.com/Lily-Technologies/lily-wallet/releases/download/v1.0.4/Lily.Wallet.Setup.1.0.4.exe"
+        href="https://github.com/Lily-Technologies/lily-wallet/releases/download/v1.0.6/Lily.Wallet.Setup.1.0.6.exe"
         className="col-span-1 flex justify-center py-8 px-2 sm:px-8 bg-gray-50 flex-col items-center mx-4 my-4 shadow-md rounded-2xl hover:-translate-y-1 transform duration-500 ease-in-out"
       >
         <div>
@@ -17,12 +17,12 @@ const DownloadButtons = () => {
         <div className="flex flex-col mt-4 items-center justify-center">
           <span>Windows</span>
           <span className="text-gray-500 text-xxs sm:text-xs">
-            Lily.Wallet.Setup.1.0.4.exe
+            Lily.Wallet.Setup.1.0.6.exe
           </span>
         </div>
       </a>
       <a
-        href="https://github.com/Lily-Technologies/lily-wallet/releases/download/v1.0.4/Lily.Wallet-1.0.4.AppImage"
+        href="https://github.com/Lily-Technologies/lily-wallet/releases/download/v1.0.6/Lily.Wallet-1.0.6.AppImage"
         className="col-span-1 flex justify-center py-8 px-2 sm:px-8 bg-gray-50 flex-col items-center mx-4 my-4 shadow-md rounded-2xl hover:-translate-y-1 transform duration-500 ease-in-out"
       >
         <div>
@@ -31,12 +31,12 @@ const DownloadButtons = () => {
         <div className="flex flex-col mt-4 items-center justify-center">
           <span>Linux</span>
           <span className="text-gray-500 text-xxs sm:text-xs">
-            Lily.Wallet-1.0.4.AppImage
+            Lily.Wallet-1.0.6.AppImage
           </span>
         </div>
       </a>
       <a
-        href="https://github.com/Lily-Technologies/lily-wallet/releases/download/v1.0.4/Lily.Wallet-1.0.4.dmg"
+        href="https://github.com/Lily-Technologies/lily-wallet/releases/download/v1.0.6/Lily.Wallet-1.0.6.dmg"
         className="col-span-1 flex justify-center py-8 px-2 sm:px-8 bg-gray-50 flex-col items-center mx-4 my-4 shadow-md rounded-2xl hover:-translate-y-1 transform duration-500 ease-in-out"
       >
         <div>
@@ -45,7 +45,7 @@ const DownloadButtons = () => {
         <div className="flex flex-col mt-4 items-center justify-center">
           <span>Mac</span>
           <span className="text-gray-500 text-xxs sm:text-xs">
-            Lily.Wallet-1.0.4.dmg
+            Lily.Wallet-1.0.6.dmg
           </span>
         </div>
       </a>
