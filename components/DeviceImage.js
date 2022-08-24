119ded1419647df6d53394bcf1a6bf1965333989
Kevin Mulcrone <KayBeSee@gmail.com>
2022-08-23 22:53:31 -0600

    feat(Share): style page

---
+++ components/DeviceImage.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { QuestionMarkCircleIcon } from "@heroicons/react/24/outline";
+
+const Coldcard = "/devices/coldcard.png";
+const LedgerNanoS = "/devices/ledger_nano_s.png";
+const LedgerNanoX = "/devices/ledger_nano_x.png";
+const TrezorOne = "/devices/trezor_1.png";
+const TrezorT = "/devices/trezor_t.png";
+const Cobo = "/devices/cobo.png";
+const Bitbox = "/devices/bitbox02.png";
+const Iphone = "/devices/iphone.png";
+const Unchained = "/devices/unchained.png";
+
+export const DeviceImage = ({ device, className }) => {
+  if (device.type === "unknown") {
+    return (
+      <div className="flex items-center justify-center py-20 px-10 text-slate-600 bg-slate-100 rounded">
+        <QuestionMarkCircleIcon className="h-10 w-10" />
+      </div>
+    );
+  }
+  return (
+    <img
+      className={className}
+      src={
+        device.type === "coldcard"
+          ? Coldcard
+          : device.type === "ledger" && device.model === "ledger_nano_s"
+          ? LedgerNanoS
+          : device.type === "ledger" && device.model === "ledger_nano_x"
+          ? LedgerNanoX
+          : device.type === "trezor" && device.model === "trezor_1"
+          ? TrezorOne
+          : device.type === "trezor" && device.model === "trezor_t"
+          ? TrezorT
+          : device.type === "trezor"
+          ? TrezorOne
+          : device.type === "cobo"
+          ? Cobo
+          : device.type === "bitbox02"
+          ? Bitbox
+          : device.type === "unchained"
+          ? Unchained
+          : Iphone
+      }
+    />
+  );
+};
+
+export default DeviceImage;
