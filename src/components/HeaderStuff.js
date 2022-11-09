78c46f9bf77ca2f328a96a31bc7c6a5004449398
Kevin Mulcrone <KayBeSee@gmail.com>
2022-11-08 23:42:54 -0700

    feat: favicon, fix build

--- src/components/HeaderStuff.js
+++ src/components/HeaderStuff.js
@@ -1,7 +1,6 @@
 const HeaderStuff = () => {
   return (
     <>
-      <link rel="icon" href="/favicon.ico" />
       <link
         href="https://fonts.googleapis.com/css?family=Montserrat:wght@100;200;500;700;800&family=Raleway:wght@100;200;500;800&display=swap"
         rel="stylesheet"
@@ -9,22 +8,21 @@ const HeaderStuff = () => {
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="./apple-touch-icon.png"
+        href="/favicon/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="./favicon-32x32.png"
+        href="/favicon/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="./favicon-16x16.png"
+        href="/favicon/favicon-16x16.png"
       />
-      <link rel="manifest" href="./site.webmanifest" />
-      <link rel="mask-icon" href="./safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
       <meta
