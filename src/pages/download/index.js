0eb15806a27fc89f53913054b4677ebb36bc53e5
Kevin Mulcrone <KayBeSee@gmail.com>
2022-11-09 22:22:04 -0700

    feat: update download page

--- src/pages/download/index.js
+++ src/pages/download/index.js
@@ -1,205 +1,96 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Head from "next/head";
-import Link from "next/link";
+import Image from "next/image";
 
 import Navigation from "../../components/Navigation";
 import Footer from "../../components/Footer";
 import HeaderStuff from "../../components/HeaderStuff";
-import DownloadButtons from "../../components/DownloadButtons";
 
 import TwitterImage from "@/images/v1.9/header.png";
 
+const downloadLink = {
+  Windows:
+    "https://github.com/Lily-Technologies/lily-wallet/releases/download/v1.1.0/Lily.Wallet.Setup.1.1.0.exe",
+  MacOS:
+    "https://github.com/Lily-Technologies/lily-wallet/releases/download/v1.1.0/Lily.Wallet-1.1.0-arm64.dmg",
+  UNIX: "https://github.com/Lily-Technologies/lily-wallet/releases/tag/v1.1.0",
+  Linux:
+    "https://github.com/Lily-Technologies/lily-wallet/releases/download/v1.1.0/Lily.Wallet-1.1.0.AppImage",
+};
+
 const Download = () => {
+  const [operatingSystem, setOperatingSystem] = useState("");
+
+  useEffect(() => {
+    if (navigator.appVersion.indexOf("Win") != -1) {
+      setOperatingSystem("Windows");
+    }
+    if (navigator.appVersion.indexOf("Mac") != -1) {
+      setOperatingSystem("MacOS");
+    }
+    if (navigator.appVersion.indexOf("X11") != -1) {
+      setOperatingSystem("UNIX");
+    }
+    if (navigator.appVersion.indexOf("Linux") != -1) {
+      setOperatingSystem("Linux");
+    }
+  }, []);
+
   return (
-    <>
-      <div class="relative pt-6 ">
-        <Head>
-          <title>Download - Lily Wallet</title>
-          <HeaderStuff />
-          <meta name="twitter:card" content="summary_large_image" />
-          <meta name="twitter:site" content="@LilyWallet" />
-          <meta name="twitter:title" content="Lily Wallet - Download v1.1" />
-          <meta
-            name="twitter:description"
-            content="Bitcoin wallet designed for everyone on their journey towards financial freedom."
-          />
-          <meta
-            name="twitter:image"
-            content={`https://lily-wallet.com${TwitterImage.src}`}
-          />
-        </Head>
-        <Navigation />
+    <div className="h-screen relative overflow-hidden overflow-y-auto">
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black">
+        <Image
+          src="/scenic.jpg"
+          layout="fill"
+          objectFit="cover"
+          priority
+          className="z-[-2]"
+        />
+      </div>
+      <Head>
+        <title>Download - Lily Wallet</title>
+        <HeaderStuff />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@LilyWallet" />
+        <meta name="twitter:title" content="Lily Wallet - Download v1.1" />
+        <meta
+          name="twitter:description"
+          content="Bitcoin wallet designed for everyone on their journey towards financial freedom."
+        />
+        <meta
+          name="twitter:image"
+          content={`https://lily-wallet.com${TwitterImage.src}`}
+        />
+      </Head>
+      <div class="relative py-6 z-10">
+        <Navigation darkBg />
       </div>
 
-      <div className="bg-white">
-        <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-24 lg:px-8">
-          <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 mb-8 sm:text-5xl sm:leading-none md:text-6xl">
+      <div className="relative text-center bg-cover h-screen flex items-center justify-center pb-28 z-10">
+        <div className="max-w-3xl mx-auto py-12 px-6 w-full">
+          <div className="bg-black p-6 rounded-2xl max-w-7xl w-fit mx-auto bg-gradient-to-br to-slate-100 from-slate-300 mb-12">
+            <div className="w-24 h-24 mx-auto relative">
+              <Image layout="fill" src="/logo.svg" />
+            </div>
+          </div>
+          <h1 className="text-4xl tracking-tight leading-10 font-semibold font-sans text-white my-4 sm:leading-none md:text-5xl">
             Download Lily Wallet
             <br />
-            <span class="text-green-600">
-              and start securing your bitcoin today
-            </span>
+          </h1>
+          <h2 className="text-md md:text-lg text-slate-100 mb-8">
+            Take control of your bitcoin today.
           </h2>
-          <DownloadButtons />
+          <a
+            href={downloadLink[operatingSystem]}
+            className="py-4 px-8 bg-emerald-600 rounded-xl text-white font-semibold font-sans hover:bg-emerald-500 transition-all duration-300"
+          >
+            Download for {operatingSystem}
+          </a>
         </div>
       </div>
 
-      <div className="bg-white">
-        <div className="pt-12 sm:pt-16 lg:pt-20">
-          <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-            <div className="text-center">
-              <h2 className="text-3xl leading-9 font-extrabold text-gray-900 sm:text-4xl sm:leading-10 lg:text-5xl lg:leading-none">
-                Hand Holding
-              </h2>
-              <p className="mt-4 text-xl leading-7 text-gray-600">
-                Need help? We will help you create your vault and test sending
-                money to and from it.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="mt-8 bg-gray-50 pb-16 sm:mt-12 sm:pb-20 lg:pb-28">
-          <div className="relative">
-            <div className="absolute inset-0 h-1/2 bg-white"></div>
-            <div className="relative max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
-              <div className="max-w-lg mx-auto rounded-lg shadow-lg overflow-hidden lg:max-w-none lg:flex">
-                <div className="bg-white px-6 py-8 lg:flex-shrink-1 lg:p-12">
-                  <h3 className="text-2xl leading-8 font-extrabold text-gray-900 sm:text-3xl sm:leading-9">
-                    Setup Walkthrough
-                  </h3>
-                  <p className="mt-6 text-base leading-6 text-gray-500">
-                    We understand that sending and receiving Bitcoin for the
-                    first time is intimidating. We will walk you through using
-                    Lily step by step so you feel comfortable securing your
-                    money and achieving true financial sovereignty.
-                  </p>
-                  <div className="mt-8">
-                    <div className="flex items-center">
-                      <h4 className="flex-shrink-0 pr-4 bg-white text-sm leading-5 tracking-wider font-semibold uppercase text-yellow-400">
-                        What's included
-                      </h4>
-                      <div className="flex-1 border-t-2 border-gray-200"></div>
-                    </div>
-                    <ul className="mt-8 lg:grid lg:grid-cols-2 lg:col-gap-8 lg:row-gap-5">
-                      <li className="flex items-start lg:col-span-1">
-                        <div className="flex-shrink-0">
-                          <svg
-                            className="h-5 w-5 text-green-400"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                        </div>
-                        <p className="ml-3 text-sm leading-5 text-gray-700">
-                          30 minute Zoom call
-                        </p>
-                      </li>
-                      <li className="flex items-start lg:col-span-1">
-                        <div className="flex-shrink-0">
-                          <svg
-                            className="h-5 w-5 text-green-400"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                        </div>
-                        <p className="ml-3 text-sm leading-5 text-gray-700">
-                          1-on-1 Personal Session
-                        </p>
-                      </li>
-                      <li className="flex items-start lg:col-span-1">
-                        <div className="flex-shrink-0">
-                          <svg
-                            className="h-5 w-5 text-green-400"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                        </div>
-                        <p className="ml-3 text-sm leading-5 text-gray-700">
-                          Vault Setup
-                        </p>
-                      </li>
-                      <li className="flex items-start lg:col-span-1">
-                        <div className="flex-shrink-0">
-                          <svg
-                            className="h-5 w-5 text-green-400"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                        </div>
-                        <p className="ml-3 text-sm leading-5 text-gray-700">
-                          Mobile Wallet Setup
-                        </p>
-                      </li>
-                      <li className="flex items-start lg:col-span-1">
-                        <div className="flex-shrink-0">
-                          <svg
-                            className="h-5 w-5 text-green-400"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                              clipRule="evenodd"
-                            />
-                          </svg>
-                        </div>
-                        <p className="ml-3 text-sm leading-5 text-gray-700">
-                          Test Send and Receive
-                        </p>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-                <div className="py-8 px-6 text-center bg-gray-50 lg:flex-shrink-0 lg:flex lg:flex-col lg:justify-center lg:p-12">
-                  <p className="text-lg leading-6 font-medium text-gray-900">
-                    30 minute Zoom call
-                  </p>
-                  <div className="mt-4 flex items-center justify-center text-5xl leading-none font-extrabold text-gray-900">
-                    <span>Free</span>
-                  </div>
-
-                  <div className="mt-6">
-                    <div className="rounded-md shadow">
-                      <Link href="/setup-walkthrough">
-                        <a className="flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-gray-900 hover:bg-gray-800 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
-                          Schedule Appointment
-                        </a>
-                      </Link>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="bg-gray-50">
+      <div className="bg-gray-50 z-10 relative">
         <div className="max-w-screen-xl mx-auto pt-12 pb-16 sm:pt-16 sm:pb-20 px-4 sm:px-6 lg:pt-20 lg:pb-28 lg:px-8">
           <h2 className="text-3xl leading-9 font-extrabold text-gray-900">
             Frequently asked questions
@@ -296,9 +187,8 @@ const Download = () => {
           </div>
         </div>
       </div>
-
       <Footer />
-    </>
+    </div>
   );
 };
 
